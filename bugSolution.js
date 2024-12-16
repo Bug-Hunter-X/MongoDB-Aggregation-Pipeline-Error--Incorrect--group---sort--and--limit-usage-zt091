ccf4dd71107fd896d4aa0a7d08bac439ea1ee64b
BugHunter-X <askzenai@gmail.com>
2024-12-16 10:34:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+// Corrected aggregation pipeline
+db.collection('data').aggregate([
+  {$group: {_id: '$field', count: {$sum: 1}}}, 
+  {$sort: {count: -1}},
+  {$limit: 10}
+])
+```
